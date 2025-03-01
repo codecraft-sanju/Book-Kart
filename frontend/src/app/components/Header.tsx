4fd508ca51e4fc4067b445abbd05af143dbc71d9
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-01 12:17:11 +0530

    header

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -3,16 +3,44 @@ import Link from 'next/link';
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Search, SearchCheckIcon, User } from 'lucide-react';
-import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 //import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
 
 const Header = () => {
 
   const user = {
-    profilePicture:""
+    profilePicture: "",
+    name: "",
+    email:""
   }
-  const userPlaceholder=''
+  const userPlaceholder = ''
+  const menuItems = {
+    ...(user && user ? [
+      {
+        href: 'account/profile',
+        content: (
+          <div className='flex space-x-4 items-center p-2 border-b'>
+ <Avatar className='w-12 h-12 -ml-2 rounded-full'>
+              {user?.profilePicture ? (
+                <AvatarImage alt='user_image'></AvatarImage>
+              ) : (
+                  <AvatarFallback>{userPlaceholder}</AvatarFallback>
+              )}
+            </Avatar>
+            <div className='flex flex-col'>
+              <span className='font-semibold text-md '>
+                {user.name}
+              </span>
+              <span className='text-xs text-gray-500'>
+                {user.email}
+              </span>
+            </div>
+          </div>
+        )
+      }
+    ]:? ())
+  }
   return (
     <header className="bg-white sticky top-0 z-50 border-b">
       {/* Desktop header */}
@@ -54,15 +82,17 @@ const Header = () => {
                   {user?.profilePicture ? (
                     <AvatarImage alt='user_image'></AvatarImage>
                   ) : userPlaceholder ? (
-                    <AvatarFallback>s</AvatarFallback>
+                      <AvatarFallback>{userPlaceholder}</AvatarFallback>
                   ) : (
                     <User className='ml-2 mt-2' />
                   )}
-                  
-                 
                 </Avatar>
+                My Account
 </Button>
             </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-80 p-2 '>
+
+            </DropdownMenuContent>
           </DropdownMenu>
 
         </div>
