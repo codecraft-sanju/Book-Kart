b75543b34741397912eae0c37b450edc99add148
codecraft-sanju <sanjusirvich18@gmail.com>
2025-03-01 09:51:56 +0530

    logo add

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,24 +1,21 @@
 import Image from 'next/image';
-import { Link } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
   return (
     <header className='bg-white sticky top-0 z-50 border-b'>
-      {/* desktop header */}
+      {/* Desktop header */}
       <div className='container w-[80%] mx-auto hidden lg:flex items-center justify-between p-4'>
-        <Link href='/' className='flex items-center '>
+        <Link href='/'>
           <Image 
-            src='/images/web-logo.png' 
-            width={450} 
-            height={100} 
-            alt='web-logo' 
+            src="/images/web-logo.png"
+            width={150} 
+            height={50} 
+            alt='desktop-logo' 
             className='h-15 w-auto' 
           />
         </Link>
-        <div>
-          {/* Add any other navigation items here */}
-        </div>
       </div>
     </header>
   );
